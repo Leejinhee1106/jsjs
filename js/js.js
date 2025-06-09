d41b9ac6e08eff08336808a535c71cc6a7be2505
jini <dlwlsgml1106@gmail.com>
2025-06-09 11:53:05 +0900

    0609 test2

--- js/js.js
+++ js/js.js
@@ -1,3 +1,4 @@
+//test1
 function myDisplayer(something){
     document.getElementById('demo').innerHTML = something;
 }
@@ -8,3 +9,36 @@ function myCalculator(num1, num2, myCallback){
 }
 
 myCalculator(5, 5, myDisplayer);
+
+/* ******************************************************************* */
+
+//test2
+function myUi(something){
+    document.getElementById("pro").innerHTML = something;
+}
+let myPromise = new Promise(function(myResolve, myReject) {
+    let x = 0;
+
+    if(x == 0){
+        myResolve("OK");
+    }
+    else{
+        myReject("Error")
+    }
+});
+
+myPromise.then(
+    function(value) {myUi(value);},
+    function(error) {myUi(error);}
+)
+
+/* ******************************************************************* */
+
+//test3
+async function myUx(){
+    let myPro = new Promise(function(resolve,reject){
+        resolve("성공");
+    });
+    document.getElementById("as").innerHTML = await myPro;
+}
+myUx();
